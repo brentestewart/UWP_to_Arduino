5926c4b1cf7340bb17eb6078853676a46fa8e41e
Brent Stewart <brent@alienarc.com>
2018-09-25 14:26:39 -0500

    Initial Commit

--- ArduinoDemo/MainPage.xaml.cs
+++ ArduinoDemo/MainPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.Maker.RemoteWiring;
+using Microsoft.Maker.Serial;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,11 +25,77 @@ namespace ArduinoDemo
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
+        private RemoteDevice Arduino { get; set; }
+        private const string PotPin = "A0";
+        private const int LedPin = 9;
+        private bool IsLedOn { get; set; }
+        private string _output = "No output";
+
+        public string Output
+        {
+            get => _output;
+            set
+            {
+                if(value != _output)
+                _output = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
+            DataContext = this;
+
+            InitializeArduio();
         }
+
+        private async void InitializeArduio()
+        {
+            var devices = await UsbSerial.listAvailableDevicesAsync();
+            var uno = devices[0];
+            var connection = new UsbSerial(uno);
+            Arduino = new RemoteDevice(connection);
+
+            Arduino.DeviceReady += Setup;
+            Arduino.DeviceConnectionFailed += Failure;
+            Arduino.AnalogPinUpdated += PotUpdated;
+            connection.begin(57600, SerialConfig.SERIAL_8N1);
+        }
+
+        private async void PotUpdated(string pin, ushort value)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                () =>
+                {
+                   Output = value.ToString();
+                });
+        }
+
+        private void Failure(string message)
+        {
+        }
+
+        private void Setup()
+        {
+            Arduino.pinMode(PotPin, PinMode.ANALOG);
+            Arduino.pinMode(LedPin, PinMode.OUTPUT);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IsLedOn = !IsLedOn;
+            var state = IsLedOn ? PinState.HIGH : PinState.LOW;
+            Arduino.digitalWrite(LedPin, state);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
